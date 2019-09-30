e2fe95b4928520f2cc47162131fa5935ce540ecc
quang anh <gitcho999@gmail.com>
2019-09-30 20:56:46 +0700

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php 
+ $hostname = "localhost";
+ $database = "test";
+ $username = "root";
+ $password = "";
+ $database = "test";
+ 
+ $connect = mysqli_connect($hostname, $username, $password, $database);
+ mysqli_query($connect, "SET NAME 'uf8'");
+ if ($connect) {
+ 	echo 'Success';
+ }else {
+ 	echo 'Fail';
+ }
+
+ ?>
