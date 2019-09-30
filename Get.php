222237dd694de789bd83e9d0b1684e0dc2ec0b04
quang anh <gitcho999@gmail.com>
2019-09-30 21:34:41 +0700

    Update Get.php

--- Get.php
+++ Get.php
@@ -1,8 +1,17 @@
 <?php 
 require 'connect.php';
 $query = "SELECT * FROM student";
+Class Student {
+	function Student($id, $name, $diachi, $namsinh) {
+		$this->id = $id;
+		$this->name = $name;
+		$this->diachi = $diachi;
+		$this->namsinh = $namsinh;
+	}
+}
 $data = mysqli_query($connect, $query);
+$array_stuent = array();
 while ($row = mysqli_fetch_assoc($data)) {
-    echo $row['hoten'] .'-'. $row['diachi'] . '-' . $row['namsinh'] . '</br>';
+    array_push($array_stuent, new Student($row['id'], $row['hoten'], $row['diachi'], $row['namsinh']));
 }
  ?>
